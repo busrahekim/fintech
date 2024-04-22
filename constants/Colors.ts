5a782f3ae27bdacd8e0c871073d6ff40d5067573
bush <busra_hekim17@outlook.com>
2024-04-22 11:09:22 +0300

    app setup

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,70 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+  primary: "#3D38ED",
+  primaryMuted: "#C9C8FA",
+  background: "#F5F5F5",
+  dark: "#141518",
+  gray: "#626D77",
+  lightGray: "#D8DCE2",
 };
+
+import { StyleSheet } from "react-native";
+import Colors from "@/constants/Colors";
+
+export const defaultStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    padding: 16,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: "700",
+  },
+  pillButton: {
+    padding: 10,
+    height: 60,
+    borderRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textLink: {
+    color: Colors.primary,
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  descriptionText: {
+    fontSize: 18,
+    marginTop: 20,
+    color: Colors.gray,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  pillButtonSmall: {
+    paddingHorizontal: 20,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonTextSmall: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  sectionHeader: {
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 20,
+    marginBottom: 10,
+  },
+  block: {
+    marginHorizontal: 20,
+    padding: 14,
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    gap: 20,
+  },
+});
