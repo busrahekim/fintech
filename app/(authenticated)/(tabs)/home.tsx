89276e47b1b86f0fb4e0df4b38151369518b4e3d
bush <busra_hekim17@outlook.com>
2024-04-25 12:34:32 +0300

    managing state with Zustand and MMKV

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -3,11 +3,21 @@ import React from "react";
 import Colors from "@/constants/Colors";
 import RoundBtn from "@/components/RoundBtn";
 import Dropdown from "@/components/Dropdown";
+import { useBalanceStore } from "@/store/balanceStore";
+import { Ionicons } from "@expo/vector-icons";
 
 const Page = () => {
-  const balance = "234";
+  const { balance, clearTransactions, runTransaction, transactions } =
+    useBalanceStore();
   // const headerHeight = useHeaderHeight();
-  const onAddMoney = () => {};
+  const onAddMoney = () => {
+    runTransaction({
+      transactionId: Math.random().toString(),
+      title: "Money added",
+      amount: Math.floor(Math.random() * 1000) * (Math.random() > 0.5 ? 1 : -1),
+      date: new Date(),
+    });
+  };
   return (
     <ScrollView
       style={{ backgroundColor: Colors.background }}
@@ -17,7 +27,7 @@ const Page = () => {
     >
       <View className="m-20">
         <View className="flex flex-row gap-2 justify-center items-end">
-          <Text className="font-bold text-5xl">{balance}</Text>
+          <Text className="font-bold text-5xl">{balance()}</Text>
           <Text className="text-xl font-semibold">$</Text>
         </View>
       </View>
@@ -26,6 +36,36 @@ const Page = () => {
         <RoundBtn icon={"refresh"} onPress={onAddMoney} title="Exchange" />
         <RoundBtn icon={"list"} onPress={onAddMoney} title="Details" />
         <Dropdown />
+      </View>
+
+      <Text className="font-semibold text-lg m-5 mb-2">Transactions</Text>
+      <View className="rounded-2xl bg-white mx-5 p-3">
+        {transactions.length === 0 && (
+          <Text style={{ padding: 14, color: Colors.gray }}>
+            No transactions yet
+          </Text>
+        )}
+        {transactions.map((transaction) => (
+          <View
+            className="items-center flex flex-row gap-4"
+            key={transaction.transactionId}
+          >
+            <View className="rounded-full w-8 h-8 bg-[#D8DCE2] justify-center items-center">
+              <Ionicons
+                size={24}
+                color={Colors.dark}
+                name={transaction.amount > 0 ? "add" : "remove"}
+              />
+            </View>
+            <View className="flex-1">
+              <Text className="font-normal">{transaction.title}</Text>
+              <Text style={{ color: Colors.gray, fontSize: 12 }}>
+                {transaction.date.toLocaleString()}
+              </Text>
+            </View>
+            <Text>{transaction.amount}$</Text>
+          </View>
+        ))}
       </View>
     </ScrollView>
   );
